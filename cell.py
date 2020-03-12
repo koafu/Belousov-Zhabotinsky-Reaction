1764a1dc7197540d59a9304f0088739d73401504
Kofi <52014643+kofiekue@users.noreply.github.com>
2020-03-12 19:20:56 +0000

    Initial commit

---
+++ cell.py
@@ -0,0 +1,49 @@
+import pygame
+import random
+
+class Cell:
+
+    def __init__(self, x, y, state):
+        self.x = x
+        self.y = y
+        self.state = state
+
+    def __repr__(self):
+        return 'Cell({})'.format(self.state)
+
+    def show(self, screen, q):
+        pass
+
+    def get_neighbors(self, grid):
+        neighbors = []
+        for x in range(-1, 2, 1):
+            for y in range(-1, 2, 1):
+                try:
+                    if grid[self.x + x][self.y + y] == self:
+                        continue
+                    neighbors.append(grid[self.x + x][self.y + y])
+
+                except IndexError:
+                    continue
+
+        return neighbors
+
+    def check_neighbors(self, grid, q, g, k1, k2):
+        self.info = {}
+
+        a = 0
+        b = 0
+        for cell in self.get_neighbors(grid):
+            if cell.state >= 2 and cell.state < q:
+                a += 1
+            if cell.state == q:
+                b += 1
+        self.info['v'] = a/k1 + b/k2 + 1
+
+        c = 0
+        S = self.state
+        for cell in self.get_neighbors(grid):
+            S += cell.state
+            if cell.state == 1:
+                c += 1
+        self.info['vi'] = S / (9-c) + g
---
+++ main.py
@@ -0,0 +1,86 @@
+import pygame
+from cell import Cell
+import random
+import time
+
+class Reaction:
+
+    def __init__(self):
+        pygame.init()
+        self.width = 400
+        self.height = 400
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption("Belousov Reaction")
+        self.clock = pygame.time.Clock()
+        self.ticks = 60
+        self.exit = False
+
+        self.q = random.randint(2, 255)
+        self.k1 = random.randint(1,8)
+        self.k2 = random.randint(1,8)
+        self.g = random.randint(1,100)
+
+        self.grid = [[] for n in range(self.width)]
+        for x in range(self.width):
+            for y in range(self.height):
+                state = random.randint(1, self.q + 1)
+                self.grid[x].append(Cell(x, y, state))
+
+    def update_window(self, pxarray):
+        for x in range(self.width):
+            for y in range(self.height):
+                color = self.translate(self.grid[x][y].state, 1, self.q, 1, 255)
+                pxarray[x,y] = int(color), int(color), int(color)
+
+    def next_generation(self):
+        for cols in self.grid:
+            for cell in cols:
+                cell.check_neighbors(self.grid, self.q, self.g, self.k1, self.k2)
+
+        for cols in self.grid:
+            for cell in cols:
+                if cell.state == self.q:
+                    cell.state = 1
+                elif cell.state == 1:
+                    cell.state = cell.info['v']
+                else:
+                    cell.state = cell.info['vi']
+
+                if cell.state > self.q:
+                    cell.state = self.q
+
+    def translate(self, value, leftMin, leftMax, rightMin, rightMax):
+        # Figure out how 'wide' each range is
+        leftSpan = leftMax - leftMin
+        rightSpan = rightMax - rightMin
+
+        # Convert the left range into a 0-1 range (float)
+        valueScaled = float(value - leftMin) / float(leftSpan)
+
+        # Convert the 0-1 range into a value in the right range.
+        return rightMin + (valueScaled * rightSpan)
+
+    def run(self):
+
+        pxarray = pygame.PixelArray(self.screen)
+
+        while not self.exit:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.exit = True
+
+            self.screen.fill((255,255,255))
+
+            self.next_generation()
+            self.update_window(pxarray)
+            print(self.grid[4][4].state)
+
+            pygame.display.flip()
+            self.clock.tick(self.ticks)
+
+        pygame.quit()
+
+if __name__ == '__main__':
+    reaction = Reaction()
+    reaction.run()
